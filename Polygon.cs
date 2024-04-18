813cb52e5c6d938aa0494de042e6759f8d7d2393
kblpz11 <167462890+kblpz11@users.noreply.github.com>
2024-04-18 12:50:02 -0500

    Add files via upload

---
+++ Polygon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Module2CodingChallenge
+{
+     class Polygon
+    {
+        // ATTRIBUTES WITH GETTERS AND SETTERS
+        public int NumberOfSides {  get; set; }
+
+        // CONSTRUCTORS
+        public Polygon() 
+        {
+            NumberOfSides = 0;
+
+        } // end constructor
+
+        public Polygon(int numberOfSides) 
+        { 
+        
+            NumberOfSides = numberOfSides;
+
+        } // end constructor
+
+    } // end class
+} // end namespace
---
+++ Square.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2CodingChallenge
+{
+    class Square : Polygon
+    {
+
+        // ATTRIBUTES WITH GETTERS AND SETTERS
+        public float Size { get; set; }
+
+        // CONSTRUCTOR
+        public Square(float size) 
+        {
+        
+            Size = size; // Set value of atttribute to the value of the argument passed into the constructor
+            NumberOfSides = 4; // Sets the value of the attribute that was inherited from the parent Polygon class
+        } // end constructor
+
+
+    } // end class
+
+} // end namespace
